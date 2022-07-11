d561b00ef5d2b76ad91e71502ad5ea13c6843dd1
Line-em <alineemily@hotmail.com>
2022-07-11 22:28:53 +0100

    Fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,10 @@ export default function Navbar() {
 		<nav className="flex items-center justify-between p-4 z-50 absolute w-full">
 			<h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
 			<div>
-				<button className="text-white px-4 cursor-pointer">SIGN IN</button>
-				<button className="bg-red-600 text-white px-4 py-2 rounded cursor-pointer">
+				<button className="bg-neutral-800 text-white px-4 py-2 rounded cursor-pointer hover:bg-neutral-900 hover:-translate-y-1 transition ease-in-out duration-300">
+					SIGN IN
+				</button>
+				<button className="bg-red-600 text-white px-4 py-2 rounded cursor-pointer  hover:bg-red-800 hover:-translate-y-1 transition ease-in-out duration-300">
 					SIGN UP
 				</button>
 			</div>
