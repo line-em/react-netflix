83d3b7c670bcce911b4423cf1788996e5ede80fd
Line-em <alineemily@hotmail.com>
2022-07-11 13:52:59 +0100

    Added most dependencies, started coding

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+	return (
+		<header>
+			<h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+			<div>
+				<button>SIGN IN</button>
+				<button>SIGN UP</button>
+			</div>
+		</header>
+	);
+}
