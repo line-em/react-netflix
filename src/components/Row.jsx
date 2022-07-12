087aae6e09820b3dc4699b125e3b6bf1cb49c7ad
Line-em <alineemily@hotmail.com>
2022-07-12 19:03:18 +0100

    Refactor Row, created new element Movie

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 export default function Row(props) {
-	const [row, setRow] = React.useState();
+	const [row, setRow] = useState();
 
 	useEffect(() => {
 		axios
@@ -15,29 +16,13 @@ export default function Row(props) {
 			});
 	}, [props.apiUrl]);
 
-	const mappedRow = row?.map((movie) => (
-		<div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 hover:scale-105 transition ease duration-300">
-			<img
-				src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
-				alt={movie?.title}
-				className="w-full h-auto block rounded"
-			/>
-			<div className="absolute top-0 left-0 w-full h-full hover:bg-black/70 opacity-0 hover:opacity-100 text-white p-2 transition ease duration-300">
-				<p className="p-2 text-base md:text-lg whitespace-normal font-bold flex justify-center h-full text-center tracking-wide">
-					{movie?.title}
-				</p>
-			</div>
-		</div>
-	));
+	const renderRow = row?.map((movie, id) => <Movie movie={movie} id={id} />);
 
 	return (
 		<>
 			<h2 className="text-white font-bold md:text-xl p-4">{props.title}</h2>
 			<div className="relative flex items-center">
-				{/* {row !== undefined && (
-					<img src={`https://image.tmdb.org/t/p/w500/${row[9].backdrop_path}`} alt="" />
-				)} */}
-				<div id={"slider"}>{mappedRow?.length > 0 && mappedRow}</div>
+				<div id={"slider"}>{renderRow?.length > 0 && renderRow}</div>
 			</div>
 		</>
 	);
