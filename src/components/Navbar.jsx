fc2581a72c304aaf2211fb03bfc6670692340f19
Line-em <alineemily@hotmail.com>
2022-07-13 12:04:57 +0100

    Add active state to buttons, fix transitions accordingly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ export default function Navbar() {
 		<nav className="flex items-center justify-between p-4 z-50 absolute w-full">
 			<h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
 			<div className="flex justify-end flex-wrap">
-				<button className="bg-neutral-800 text-white px-4 py-2 rounded cursor-pointer hover:bg-neutral-900 hover:-translate-y-1 transition ease-in-out duration-300">
+				<button className="bg-neutral-800 text-white px-4 py-2 rounded cursor-pointer hover:bg-neutral-900 hover:-translate-y-1 transition active:-translate-y-0">
 					SIGN IN
 				</button>
-				<button className="bg-red-600 text-white px-4 py-2 rounded cursor-pointer  hover:bg-red-800 hover:-translate-y-1 transition ease-in-out duration-300">
+				<button className="bg-red-600 text-white px-4 py-2 rounded cursor-pointer  hover:bg-red-800 hover:-translate-y-1 transition active:-translate-y-0">
 					SIGN UP
 				</button>
 			</div>
