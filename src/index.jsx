96556081b70aa52cc17b88eba8149de72ac0bf44
Line-em <alineemily@hotmail.com>
2023-01-25 12:06:02 +0000

    notes

--- src/index.jsx
+++ src/index.jsx
@@ -7,9 +7,7 @@ import "@fontsource/rubik";
 import "@fontsource/rubik/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	// <HashRouter basename="/react-netflix">
 	<HashRouter>
-		{/*  The basename ensures it will maintain the route, but it does not work on dev. */}
 		<App />
 	</HashRouter>
 );
